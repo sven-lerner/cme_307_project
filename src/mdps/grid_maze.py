43c10c0531f439bb7bcb4d477ac4771a19271ac0
Tetiana <theverybigbang@gmail.com>
2020-03-15 12:09:15 -0700

    added action spaces of size 1,2,8; new plots in notebook

--- src/mdps/grid_maze.py
+++ src/mdps/grid_maze.py
@@ -3,20 +3,29 @@ import numpy as np
 
 class RandomGridMaze:
 
-    def __init__(self, x_dim, y_dim, num_terminal, seed=None, deterministic=True):
+    def __init__(self, x_dim, y_dim, num_terminal, num_actions=8, seed=None, deterministic=True):
         self.state_actions, self.state_transitions, self.state_rewards, self.terminal_states = \
-            self._get_maze(x_dim, y_dim, num_terminal, seed=seed, deterministic=deterministic)
+            self._get_maze(x_dim, y_dim, num_terminal, num_actions=num_actions, seed=seed, deterministic=deterministic)
 
     def get_mdp(self):
         return self.state_actions, self.state_transitions, self.state_rewards, self.terminal_states
 
-    def _get_maze(self, x_dim, y_dim, num_terminal, seed=None, deterministic=True):
+    def _get_maze(self, x_dim, y_dim, num_terminal, num_actions=8, seed=None, deterministic=True):
         if seed is not None:
             np.random.seed(seed)
         states = [(x, y) for x in range(x_dim) for y in range(y_dim)]
         terminal_states_idx = np.random.choice(list(range(len(states))), size=num_terminal)
         terminal_states = [states[idx] for idx in terminal_states_idx]
-        actions = {'l', 'r', 'u', 'd'}
+
+        if num_actions ==1:
+            actions = {'l'}
+        elif num_actions ==2:
+            actions = {'l', 'r'}
+        elif num_actions ==4:
+            actions = {'l', 'r', 'u', 'd'}
+        elif num_actions ==8:
+            actions = {'l', 'r', 'u', 'd', 'lu', 'ld', 'ru', 'rd'}
+
         state_transitions = {}
         state_actions = {}
         state_rewards = {}
@@ -32,7 +41,7 @@ class RandomGridMaze:
                     next_coord = None
                     if action == 'stay':
                         next_coord = state
-                    elif action == 'l':
+                    if action == 'l':
                         next_coord = (state[0], max(0, state[1] - 1))
                     if action == 'r':
                         next_coord = (state[0], min(x_dim - 1, state[1] + 1))
@@ -40,6 +49,14 @@ class RandomGridMaze:
                         next_coord = (min(y_dim - 1, state[0] + 1), state[1])
                     if action == 'd':
                         next_coord = (max(0, state[0] - 1), state[1])
+                    if action == 'lu':
+                        next_coord = (min(y_dim - 1, state[0] + 1), max(0, state[1] - 1))
+                    if action == 'ru':
+                        next_coord = (min(y_dim - 1, state[0] + 1), min(x_dim - 1, state[1] + 1))
+                    if action == 'ld':
+                        next_coord = (max(0, state[0] - 1), max(0, state[1] - 1))
+                    if action == 'rd':
+                        next_coord = (max(0, state[0] - 1),  min(x_dim - 1, state[1] + 1))
                     state_transitions[state][action] = {next_coord: 1}
                     if next_coord != state and next_coord in terminal_states:
                         state_rewards[state][action] = np.random.choice([-1, 0, 1])
